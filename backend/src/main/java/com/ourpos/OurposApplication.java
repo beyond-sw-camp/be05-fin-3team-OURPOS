afeae0f6a574a3d449f858ad3a4de6184fdecd81
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-05-17 11:05:32 +0900

    init: backend 초기 설정

---
+++ backend/src/main/java/com/ourpos/OurposApplication.java
@@ -0,0 +1,13 @@
+package com.ourpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurposApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurposApplication.class, args);
+    }
+
+}
