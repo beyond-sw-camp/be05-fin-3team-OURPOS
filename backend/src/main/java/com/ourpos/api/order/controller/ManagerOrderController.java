2855b57a1f3ac183365a8520e4452729c1773091
hana <jjina.js@gmail.com>
2024-05-21 22:23:50 +0900

    FEAT 지점용 POS     대기 주문 승인 rest api

--- backend/src/main/java/com/ourpos/api/order/controller/ManagerOrderController.java
+++ backend/src/main/java/com/ourpos/api/order/controller/ManagerOrderController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import com.ourpos.api.Result;
 import com.ourpos.api.order.dto.response.HallOrderResponseDto;
 import com.ourpos.api.order.service.ManagerOrderService;
 import com.ourpos.api.order.service.OrderQueryService;
+import com.ourpos.api.order.service.OrderService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,6 +26,8 @@ public class ManagerOrderController {
     private final OrderQueryService orderQueryService;
 
     private final ManagerOrderService managerOrderService;
+
+    private final OrderService orderService;
 
     // 대기 상태인 주문 목록 확인
     @GetMapping("/orders/{storeId}")
@@ -41,6 +45,13 @@ public class ManagerOrderController {
         HallOrderResponseDto hallOrderResponseDto = orderQueryService.findHallOrder(orderId);
 
         return new Result<>(HttpStatus.OK.value(), "지점용 POS에서, 메뉴 상세를 확인합니다.", hallOrderResponseDto);
+    }
+
+    // 대기 주문 승인 - 주문 상태 ( 대기 -> 조리중 )
+    @PutMapping("/orders/hall/accept/{orderId}")
+    public void acceptHallOrder(@PathVariable Long orderId) {
+        System.out.println("ManagerOrderController.acceptHallOrder");
+        orderService.acceptHallOrder(orderId);
     }
 
 }
