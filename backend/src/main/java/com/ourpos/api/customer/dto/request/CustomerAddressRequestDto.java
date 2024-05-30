710ecb6c8c9dc427467f9cc233948e7c4708ea47
yk <lysel1225@naver.com>
2024-05-30 10:14:53 +0900

    validation

--- backend/src/main/java/com/ourpos/api/customer/dto/request/CustomerAddressRequestDto.java
+++ backend/src/main/java/com/ourpos/api/customer/dto/request/CustomerAddressRequestDto.java
@@ -1,6 +1,7 @@
 package com.ourpos.api.customer.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,12 +11,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CustomerAddressRequestDto {
 
-
+    @NotBlank(message = "주소 이름을 입력해주세요")
     private String name;
+    @NotBlank(message = "수령하실 분의 이름을 입력해주세요")
     private String receiverName;
+    @NotBlank(message = "전화번호를 입력해주세요" )
     private String telNo;
+    @NotNull(message = "도로명 주소를 입력해주세요")
     private String addressBase;
+    @NotNull(message = "상세주소를 입력해주세요")
     private String addressDetail;
+    @NotBlank(message = "우편주소를 입력해주세요")
     private String zipcode;
 
     @Builder
--- backend/src/main/java/com/ourpos/api/storeorder/dto/request/StoreCommRequestDto.java
+++ backend/src/main/java/com/ourpos/api/storeorder/dto/request/StoreCommRequestDto.java
@@ -6,6 +6,10 @@ import com.ourpos.domain.storeorder.StoreComm;
 import com.ourpos.domain.storeorder.StoreCommCategory;
 import com.ourpos.domain.storeorder.StoreCommStatus;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -15,17 +19,36 @@ import lombok.ToString;
 @Setter
 public class StoreCommRequestDto {
 
+    @NotNull(message = "가게 상품 Id를 입력해주세요")
+    @Positive(message = "1 이상의 상수를 입력해주세요")
     private Long storeCommId;
+
+    @NotBlank(message = "가게 물품의 이름을 입력해주세요")
     private String storeCommName;
+
+    @NotBlank(message = "가게 물품의 가격을 입력해주세요")
     private Integer storeCommPrice;
+
+    @NotBlank(message = "가게 물품의 단위를 입력해주세요" )
     private String storeCommArticleUnit;
+
     private StoreCommStatus storeCommStatus;
+
+    @NotNull(message = "가게 물품의 상세 설명을 입력해주세요 ")
     private String storeCommDescription;
+
+    @NotBlank(message = "가게 물품의 종류를 입력해주세요" )
     private StoreCommCategory storeCommCategory;
+
     private String storeCommPictureUrl;
+
     private LocalDateTime storeCommCreatedDateTime;
+
     private LocalDateTime storeCommModifiedDateTime;
+
     private Boolean storeCommDeleteYn;
+
+
     private LocalDateTime storeCommDeletedDateTime;
 
     // public StoreCommRequestDto(StoreComm storeComm){
