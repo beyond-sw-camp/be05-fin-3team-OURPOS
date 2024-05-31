c14a1fdcf8764f05c5e4b91520c36bcd0f6bdf62
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-05-31 09:31:32 +0900

    feat: Category 삭제 처리 로직 수정, Customer 엔티티 이메일 칼럼 추가

--- backend/src/main/java/com/ourpos/domain/menu/Category.java
+++ backend/src/main/java/com/ourpos/domain/menu/Category.java
@@ -4,13 +4,13 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,45 +22,46 @@ import lombok.Singular;
 @Entity
 public class Category {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "category_id")
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
 
-	@Column(name = "category_name")
-	private String name;
+    @Column(name = "category_name")
+    private String name;
 
-	@Column(name = "category_deleted_yn")
-	private Boolean deletedYn;
+    @Column(name = "category_deleted_yn")
+    private Boolean deletedYn;
 
-	@Column(name = "category_deletedDateTime")
-	private LocalDateTime deletedDateTime;
+    @Column(name = "category_deleted_date_time")
+    private LocalDateTime deletedDateTime;
 
-	@OneToMany(mappedBy = "category")
-	private List<MenuOptionGroup> menuOptionGroups = new ArrayList<>();
+    @OneToMany(mappedBy = "category")
+    private List<MenuOptionGroup> menuOptionGroups = new ArrayList<>();
 
-	@Builder
-	private Category(String name, @Singular List<MenuOptionGroup> menuOptionGroups) {
-		this.name = name;
-		this.deletedYn = false;
-		for (MenuOptionGroup menuOptionGroup : menuOptionGroups) {
-			addMenuOptionGroup(menuOptionGroup);
-		}
-	}
+    @Builder
+    private Category(String name, @Singular List<MenuOptionGroup> menuOptionGroups) {
+        this.name = name;
+        this.deletedYn = false;
+        for (MenuOptionGroup menuOptionGroup : menuOptionGroups) {
+            addMenuOptionGroup(menuOptionGroup);
+        }
+    }
 
-	// 연관관계 편의 메서드
-	public void addMenuOptionGroup(MenuOptionGroup menuOptionGroup) {
-		menuOptionGroups.add(menuOptionGroup);
-		menuOptionGroup.setCategory(this);
-	}
+    // 연관관계 편의 메서드
+    public void addMenuOptionGroup(MenuOptionGroup menuOptionGroup) {
+        menuOptionGroups.add(menuOptionGroup);
+        menuOptionGroup.setCategory(this);
+    }
 
-	public void update(String name) {
-		this.name = name;
-	}
+    public void update(String name) {
+        this.name = name;
+    }
 
-	public void delete(LocalDateTime deletedDateTime) {
-		this.deletedYn = true;
-		this.deletedDateTime = deletedDateTime;
-	}
+    public void delete(LocalDateTime deletedDateTime) {
+        this.deletedYn = true;
+        this.deletedDateTime = deletedDateTime;
+        menuOptionGroups.forEach(menuOptionGroup -> menuOptionGroup.delete(deletedDateTime));
+    }
 }
 
---
+++ backend/src/test/java/com/ourpos/domain/menu/CategoryQueryRepositoryTest.java
@@ -0,0 +1,88 @@
+package com.ourpos.domain.menu;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ourpos.api.category.service.CategoryService;
+
+@Transactional
+@SpringBootTest
+class CategoryQueryRepositoryTest {
+
+    @Autowired
+    private CategoryQueryRepository categoryQueryRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @DisplayName("삭제 처리가 되지 않은 모든 카테고리를 조회할 수 있다.")
+    @Test
+    void test() {
+        // given
+        Category category1 = Category.builder()
+            .name("치킨")
+            .build();
+
+        Category category2 = Category.builder()
+            .name("피자")
+            .build();
+
+        Category category3 = Category.builder()
+            .name("사이드")
+            .build();
+
+        categoryRepository.save(category1);
+        categoryRepository.save(category2);
+        categoryRepository.save(category3);
+
+        categoryService.deleteCategory(category3.getId());
+
+        // when
+        List<Category> allCategories = categoryQueryRepository.findAllCategories();
+
+        // then
+        assertThat(allCategories).hasSize(2)
+            .extracting("name")
+            .containsExactly("치킨", "피자");
+    }
+
+    @DisplayName("삭제 처리가 되지 않은 카테고리 하나를 조회할 수 있다.")
+    @Test
+    void test2() {
+        // given
+        Category category1 = Category.builder()
+            .name("치킨")
+            .build();
+
+        Category category2 = Category.builder()
+            .name("피자")
+            .build();
+
+        Category category3 = Category.builder()
+            .name("사이드")
+            .build();
+
+        categoryRepository.save(category1);
+        categoryRepository.save(category2);
+        categoryRepository.save(category3);
+
+        categoryService.deleteCategory(category3.getId());
+
+        // when
+        Category one = categoryQueryRepository.findOne(category2.getId()).get();
+
+        // then
+        assertThat(one.getName()).isEqualTo("피자");
+    }
+
+}
