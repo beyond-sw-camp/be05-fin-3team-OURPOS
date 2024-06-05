51abe7eb2c54383e4b3de422b0fee0f35341dee7
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-06-05 12:34:29 +0900

    feat: router 설정 및 로그인 페이지

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,10 +6,18 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import {createRouter, createWebHistory} from 'vue-router';
+import LoginPage from "@/pages/LoginPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      name: 'login',
+      path: '/login',
+      component: LoginPage
+    }
+  ]
 })
 
 export default router
