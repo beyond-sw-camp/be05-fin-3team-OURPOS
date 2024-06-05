427ed972409eb79417286d09d8053c9cd9f33c2d
mhshin <tlsalsgud2@naver.com>
2024-06-05 17:53:23 +0900

    wrap up commit

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,6 +8,7 @@
 // Composables
 import {createRouter, createWebHistory} from 'vue-router';
 import LoginPage from "@/pages/LoginPage.vue";
+import StoreLandingPage from '@/pages/StroreLandingPage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,11 @@ const router = createRouter({
       name: 'login',
       path: '/login',
       component: LoginPage
+    },
+    {
+      name: 'storeLanding',
+      path: '/storeLanding',
+      component: StoreLandingPage
     }
   ]
 })
