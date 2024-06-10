372fa21fd34abf733174b7092aa49228996d778a
mhshin <tlsalsgud2@naver.com>
2024-06-10 09:51:00 +0900

    [front-end] fix : index.js에서 Toast 관련 코드 삭제

--- frontend/src/plugins/index.js
+++ frontend/src/plugins/index.js
@@ -7,11 +7,11 @@
 // Plugins
 import vuetify from './vuetify'
 import router from '@/router'
-import Toast from "vue-toastification";
+// import Toast from "vue-toastification";
 
 export function registerPlugins (app) {
   app
     .use(vuetify)
     .use(router)
-    .use(Toast)
+    // .use(Toast)
 }
