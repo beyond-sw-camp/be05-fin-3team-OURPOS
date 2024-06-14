952ffe5c753fb48a41e56975871e96b3c95b450c
hana <jjina.js@gmail.com>
2024-06-14 16:07:44 +0900

    FEAT 지점, 점주 페이지 라우터 설정

--- frontend-manager/src/router/index.js
+++ frontend-manager/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Dashboard from "../views/Dashboard.vue";
 import Tables from "../views/Tables.vue";
 import Billing from "../views/Billing.vue";
 import RTL from "../views/Rtl.vue";
@@ -7,17 +6,21 @@ import Notifications from "../views/Notifications.vue";
 import Profile from "../views/Profile.vue";
 import SignUp from "../views/SignUp.vue";
 import SubDash from "../views/SubDashboard.vue";
+import {checkUserRole} from "@/utils/auth";
+import ManagerHome from "@/views/ManagerHome.vue";
+import OwnerHome from "@/views/OwnerHome.vue";
 
 const routes = [
   {
     path: "/",
     name: "/",
-    redirect: "/dashboard",
+    redirect: "/sign-up",
   },
   {
-    path: "/dashboard",
-    name: "Dashboard",
-    component: Dashboard,
+    path: "/owner",
+    name: "OwnerHome",
+    component: OwnerHome,
+    meta: { requiredRoles: ['ROLE_SUPER_ADMIN'] }
   },
   {
     path: "/tables",
@@ -53,6 +56,13 @@ const routes = [
     path: "/subdash",
     name: "SubDash",
     component: SubDash,
+    meta: { requiredRoles: ['ROLE_SUPER_ADMIN'] }
+  },
+  {
+    path: "/manager",
+    name: "ManagerHome",
+    component: ManagerHome,
+    meta: { requiredRoles: ['ROLE_ADMIN'] }
   }
 ];
 
@@ -61,5 +71,18 @@ const router = createRouter({
   routes,
   linkActiveClass: "active",
 });
-
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiredRoles) {
+    const hasAccess = checkUserRole(to.meta.requiredRoles);
+    console.log(hasAccess);
+    if (hasAccess) {
+      next();
+    } else {
+      alert('접근 권한이 없습니다.');
+      next({ name: 'SignUp' });
+    }
+  } else {
+    next();
+  }
+});
 export default router;
