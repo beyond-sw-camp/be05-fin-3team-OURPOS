691df6816c93cbb587122c8a4fa50c545ac87498
hana <jjina.js@gmail.com>
2024-06-21 17:18:14 +0900

    REFACTOR 점주 POS     현재 재고량에서 비품 빼고 식자재만 확인

--- backend/src/main/java/com/ourpos/domain/store/StoreStockRepository.java
+++ backend/src/main/java/com/ourpos/domain/store/StoreStockRepository.java
@@ -5,11 +5,19 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.ourpos.domain.storeorder.StoreCommCategory;
 
 public interface StoreStockRepository extends JpaRepository<StoreStock, Long> {
 
     List<StoreStock> findByStoreIdAndStoreCommId(Long storeId, Long storeCommId);
-    Page<StoreStock> findAllByStoreId(Long storeId, Pageable pageable);
 
+    @Query("SELECT ss FROM StoreStock ss WHERE ss.store.id = :storeId AND ss.storeComm.category = com.ourpos.domain.storeorder.StoreCommCategory.INGREDIENT")
+    Page<StoreStock> findAllByStoreId(@Param("storeId") Long storeId, Pageable pageable);
+
+
+    // Page<StoreStock> findAllByStoreId(Long storeId, Pageable pageable);
     
 }
