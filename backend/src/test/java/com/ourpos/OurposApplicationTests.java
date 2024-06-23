992522dce5679caf26d21a8cc0d2538b196a737b
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-06-23 20:15:56 +0900

    test: test 설정 수정

--- backend/src/test/java/com/ourpos/OurposApplicationTests.java
+++ backend/src/test/java/com/ourpos/OurposApplicationTests.java
@@ -3,7 +3,7 @@ package com.ourpos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.profiles.active=test")
+@SpringBootTest(classes = OurposApplication.class)
 class OurposApplicationTests {
 
     @Test
