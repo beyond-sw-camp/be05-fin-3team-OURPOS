1517ea1f7590d1743e8f257d202f494e72e61147
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-06-23 20:01:56 +0900

    fix: OurposApplicationTests 수정

--- backend/src/test/java/com/ourpos/OurposApplicationTests.java
+++ backend/src/test/java/com/ourpos/OurposApplicationTests.java
@@ -3,7 +3,7 @@ package com.ourpos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.profiles.active:test")
+@SpringBootTest(properties = "spring.profiles.active=test")
 class OurposApplicationTests {
 
     @Test
